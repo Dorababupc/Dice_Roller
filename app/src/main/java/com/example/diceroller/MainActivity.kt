a64daf97121323ec83f0ec180e97e13818847536
Dorababu Arigi <dorababuarigi765@gmail.com>
2022-04-27 22:09:07 +0530

    Implementing the random function in the on click listener.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random as Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +13,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button : Button =findViewById(R.id.button_view)
         button.setOnClickListener{
-            Toast.makeText(this, "button is clicked", Toast.LENGTH_SHORT).show()
+            randomNumber()
         }
+    }
+
+    private fun randomNumber() {
+        val textView: TextView=findViewById(R.id.text_view);
+        textView.text=(Random.nextInt(6)+1).toString()
+
     }
 }
