4f058fea35d99dea6c11f5f03dfa6841a569fbf4
Dorababu Arigi <dorababuarigi765@gmail.com>
2022-04-27 23:20:03 +0530

    User interface for the game of 4 players

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.diceroller
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random as Random
@@ -18,8 +20,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun randomNumber() {
-        val textView: TextView=findViewById(R.id.text_view);
-        textView.text=(Random.nextInt(6)+1).toString()
-
+        val imageView: ImageView=findViewById(R.id.image_view);
+        val randomNumber=(Random.nextInt(6)+1)
+        val resoure=when(randomNumber){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else-> R.drawable.dice_6
+        }
+        imageView.setImageResource(resoure)
     }
 }
