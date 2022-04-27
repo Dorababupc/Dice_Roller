f514e1b47bd3ee529918bbb6efbfd57d49e3e67c
Dorababu Arigi <dorababuarigi765@gmail.com>
2022-04-27 21:47:40 +0530

    Adding id to the button and setting on click listener.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button : Button =findViewById(R.id.button_view)
+        button.setOnClickListener{
+            Toast.makeText(this, "button is clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
