d8adfeea514ee147b126ff8e313df5b48cb9f93f
Dorababu Arigi <dorababuarigi765@gmail.com>
2022-04-28 00:28:25 +0530

    final application for 4 players

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,16 +10,28 @@ import android.widget.Toast
 import kotlin.random.Random as Random
 
 class MainActivity : AppCompatActivity() {
+    val arrayname = arrayOf(0,0,0,0)
+
+    var i=0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button : Button =findViewById(R.id.button_view)
+
         button.setOnClickListener{
             randomNumber()
+            if(i==4){
+                i=0;
+            }
         }
+
     }
 
     private fun randomNumber() {
+        val player_name: TextView=findViewById(R.id.player_id)
+        player_name.text="Player${i+1}"
+        val player_score: TextView=findViewById(R.id.player_score)
+        player_score.text=arrayname[i].toString()
         val imageView: ImageView=findViewById(R.id.image_view);
         val randomNumber=(Random.nextInt(6)+1)
         val resoure=when(randomNumber){
@@ -31,5 +43,21 @@ class MainActivity : AppCompatActivity() {
             else-> R.drawable.dice_6
         }
         imageView.setImageResource(resoure)
+        if(arrayname[i]+randomNumber<20){
+            arrayname[i]+=randomNumber
+            player_score.text=arrayname[i].toString()
+            ++i;
+        }
+        else if(arrayname[i]+randomNumber==20){
+            arrayname[i]+=randomNumber
+            player_score.text=arrayname[i].toString()
+            Toast.makeText(this,"Congratulations , to the player ${i+1}, you are the winner!!!",Toast.LENGTH_SHORT).show();
+            i=0
+            for (i in 0..arrayname.size-1)
+            {
+                arrayname[i]=0
+            }
+        }
+
     }
 }
